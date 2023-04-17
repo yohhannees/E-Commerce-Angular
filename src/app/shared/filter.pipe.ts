af8231c00aa99f02c950027d11ce6a7faa60a8ba
Yohannees <yohannesgetachewerieso@gmail.com>
2023-04-18 02:12:15 +0300

    filter logic added

--- src/app/shared/filter.pipe.ts
+++ src/app/shared/filter.pipe.ts
@@ -4,9 +4,18 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+ transform(value : any[], filterString: string, propName:string): any[] {
+    const result:any =[];
+    if(!value || filterString==='' || propName ===''){
+      return value;
+    }
+    value.forEach((a:any)=>{
+      if(a[propName].trim().toLowerCase().includes(filterString.toLowerCase())){
+        result.push(a);
+      }
+    });
+    return result;
   }
+
 
 }
