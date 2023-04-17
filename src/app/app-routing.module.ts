2de2e039b4b0b4999f0f80c5665f5b34a41d2bc7
Yohannees <yohannesgetachewerieso@gmail.com>
2023-04-17 19:11:57 +0300

    cart image added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './component/cart/cart.component';
+import { ProductsComponent } from './component/products/products.component';
 
-
+const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'products', component: ProductsComponent },
+  { path: 'cart', component: CartComponent },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/component/products/products.component.ts
+++ src/app/component/products/products.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
+import { ApiService } from 'src/app/service/api.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  public productList: any;
+  // public filterCategory : any
+  // searchKey:string ="";
+  // constructor(private api: ApiService, private cartService: CartService) {}
+  constructor(private api: ApiService) {}
 
+
+
+  ngOnInit(): void {
+      this.api.getProduct()
+      .subscribe(res => {
+          this.productList = res;}
+      );
+  }
 }
+
