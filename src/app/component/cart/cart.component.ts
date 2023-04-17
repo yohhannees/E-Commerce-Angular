35ab59300eaac0fbf891f4b11db36b0b2f4dbdf1
Yohannees <yohannesgetachewerieso@gmail.com>
2023-04-17 21:47:01 +0300

    cart logic added

--- src/app/component/cart/cart.component.ts
+++ src/app/component/cart/cart.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/service/cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  public products: any = [];
+  public grandTotal: number=0;
+  constructor(private cartService: CartService) {}
 
+  ngOnInit(): void {
+    this.cartService.getProducts().subscribe((res) => {
+      this.products = res;
+      this.grandTotal = this.cartService.getTotalPrice();
+    });
+  }
+  removeItem(item: any) {
+    this.cartService.removeCartItem(item);
+  }
+  emptycart() {
+    this.cartService.removeAllCart();
+  }
 }
--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/service/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  constructor( private) {}
+  public totalItem: number = 0;
+  constructor(private cartService: CartService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.cartService.getProducts().subscribe((res) => {
+      this.totalItem = res.length;
+    });
+  }
 }
