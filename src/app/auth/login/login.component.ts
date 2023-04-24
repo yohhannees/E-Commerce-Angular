6b0b95679899ee192a77905785b81f4e84159107
Yohannees <yohannesgetachewerieso@gmail.com>
2023-04-24 20:13:07 +0300

    form type created

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginForm } from 'src/types/Auth';
 
 @Component({
   selector: 'app-login',
@@ -6,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  email: string="";
-
+   form:LoginForm={
+    email:'',
+    password:''
+   }
   constructor() { }
 
   ngOnInit(): void {
